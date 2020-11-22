30d216b914da1c079645cdb0f27e8d3c73e7f674
Boris <sslonovborisss@gmail.com>
2020-11-22 15:36:37 +0300

    add

--- dist/js/main.js
+++ dist/js/main.js
@@ -276,6 +276,27 @@ document.addEventListener("scroll", function (e) {
   }
 });
 
+(function () {
+  var btnScrollDown = document.querySelector('#scroll_down');
+
+  function scrollDown() {
+    var windowCoords = document.documentElement.clientHeight;
+
+    (function scroll() {
+      if (window.pageYOffset < windowCoords) {
+        window.scrollBy(0, 10);
+        setTimeout(scroll, 0);
+      }
+
+      if (window.pageYOffset > windowCoords) {
+        window.scrollTo(0, windowCoords);
+      }
+    })();
+  }
+
+  btnScrollDown.addEventListener('click', scrollDown);
+})();
+
 /***/ }),
 
 /***/ "./src/js/import/modules.js":
--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -52,3 +52,25 @@ document.addEventListener("scroll", (e) => {
         content.classList.add('header-none-bgc');
     }
 });
+
+
+
+(function() {
+  
+    var btnScrollDown = document.querySelector('#scroll_down');
+  
+    function scrollDown() {
+      var windowCoords = document.documentElement.clientHeight;
+      (function scroll() {
+        if (window.pageYOffset < windowCoords) {
+          window.scrollBy(0, 10);
+          setTimeout(scroll, 0);
+        }
+        if (window.pageYOffset > windowCoords) {
+          window.scrollTo(0, windowCoords);
+        }
+      })();
+    }
+  
+    btnScrollDown.addEventListener('click', scrollDown);
+  })();
