3594dbe01fe05ae3f73c4b3138a1a2272f48e74c
Boris <sslonovborisss@gmail.com>
2020-12-09 00:56:18 +0300

    add popup

---
+++ src/blocks/modules/popups/popups.js
@@ -0,0 +1,7 @@
+import $ from "micromodal"
+
+
+
+MicroModal.init({
+    disableScroll: true
+});
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,4 +1,5 @@
 import "%modules%/header/header";
+import "%modules%/popups/popups";
 import "%modules%/adv-slider/adv-slider.js";
 import "%modules%/products/products.js";
 import "%modules%/footer/footer";
