3f746b398ca87845f77818e9572c189b14b063af
Boris <sslonovborisss@gmail.com>
2020-12-09 06:02:38 +0300

    add page-partners

--- dist/js/main.js
+++ dist/js/main.js
@@ -169,19 +169,8 @@ __webpack_require__.r(__webpack_exports__);
 var menu = ['Slide 1', 'Slide 2', 'Slide 3'];
 var mySwiper = new Swiper('.swiper-container', {
   autoHeight: true,
-  speed: 300,
-  cubeEffect: {
-    slideShadows: false,
-    shadow: false
-  },
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    renderBullet: function renderBullet(index, className) {
-      return '<span class="' + className + '">' + menu[index] + '</span>';
-    }
-  },
+  speed: 500,
+  effect: 'fade',
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
--- src/blocks/modules/adv-slider/adv-slider.js
+++ src/blocks/modules/adv-slider/adv-slider.js
@@ -4,20 +4,8 @@ import $ from "swiper";
 var menu = ['Slide 1', 'Slide 2', 'Slide 3']
 var mySwiper = new Swiper('.swiper-container', {
     autoHeight: true,
-    speed: 300,
-    cubeEffect: {
-        slideShadows: false,
-        shadow: false
-    },
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + (menu[index]) + '</span>';
-        },
-    },
-
+    speed: 500,
+    effect: 'fade',
     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
