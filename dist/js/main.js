ca960f472f1f11f55b27390a617c89cece9fe93c
Boris <sslonovborisss@gmail.com>
2021-01-25 20:30:48 +0300

    add icon animate

--- dist/js/main.js
+++ dist/js/main.js
@@ -493,6 +493,32 @@ if (linkChanger1) {
   });
 }
 
+(function () {
+  var item1 = document.querySelector('.bed-review__item_2');
+  var dot1 = document.querySelector('.bed-dot_1');
+
+  function showDot1() {
+    dot1.classList.add('dot-active');
+  }
+
+  function hideDot1() {
+    dot1.classList.remove('dot-active');
+  }
+
+  function activeIconDot() {
+    item1.classList.add('dot-icon-active');
+  }
+
+  function deactiveIconDot() {
+    item1.classList.remove('dot-icon-active');
+  }
+
+  item1.onmouseover = showDot1;
+  item1.onmouseleave = hideDot1;
+  dot1.onmouseover = activeIconDot;
+  dot1.onmouseleave = deactiveIconDot;
+})();
+
 /***/ }),
 
 /***/ "./src/js/import/modules.js":
--- src/blocks/modules/products/products.js
+++ src/blocks/modules/products/products.js
@@ -36,3 +36,33 @@ if (linkChanger1) {
         changingImg2.classList.add("products__img_active")
     });
 }
+
+
+
+(function () {
+
+    let item1 = document.querySelector('.bed-review__item_2');
+    let dot1 = document.querySelector('.bed-dot_1');
+
+    function showDot1() {
+        dot1.classList.add('dot-active')
+    }
+
+    function hideDot1() {
+        dot1.classList.remove('dot-active')
+    }
+
+    function activeIconDot() {
+        item1.classList.add('dot-icon-active')
+    }
+
+    function deactiveIconDot() {
+        item1.classList.remove('dot-icon-active')
+    }
+
+    item1.onmouseover = showDot1;
+    item1.onmouseleave = hideDot1;
+    dot1.onmouseover = activeIconDot;
+    dot1.onmouseleave = deactiveIconDot;
+
+})();
